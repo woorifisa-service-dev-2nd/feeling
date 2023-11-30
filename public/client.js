32918f5783c6ec41f3d8b0c391314f5b68e3c3cf
JohnDohnut <lgj1089@gmail.com>
2023-11-30 15:11:32 +0900

    update

--- public/client.js
+++ public/client.js
@@ -1,11 +1,24 @@
-const target_name = document.getElementById("Target_Name");
-const comment = document.getElementById("Comment");
-const submit = document.getElementById("POST");
-const serverURL = "something";
+const targetName = document.getElementById('Target_Name');
+const comment = document.getElementById('Comment');
+const submit = document.getElementById('POST');
+const serverURL = '/submitComment';
 
-function postData(e){
-
-    //do something
+function postData(event) {
+  event.preventDefault();
+  const requestMessage = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: targetName.value,
+      comment: comment.value,
+    }),
+  };
+  console.log(JSON.stringify(requestMessage));
+  fetch(serverURL, requestMessage)
+    .then((response) => response.json())
+    .then((data) => console.log('response:', data))
+    .catch((error) => console.log(error));
 }
-submit.addEventListener("click", postData);
-
+submit.addEventListener('click', postData);
