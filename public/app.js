e1da25fd727c3381a0755a297a8addb8355c1d07
JohnDohnut <lgj1089@gmail.com>
2023-11-30 11:55:29 +0900

    add

--- public/app.js
+++
@@ -1,5 +0,0 @@
-let score1 = 10;
-let score2 = 20;
-const totalScore = new String(score1 + score2);
-
-console.log(totalScore);
---
+++ public/client.js
@@ -0,0 +1,11 @@
+const target_name = document.getElementById("Target_Name");
+const comment = document.getElementById("Comment");
+const submit = document.getElementById("POST");
+const serverURL = "something";
+
+function postData(e){
+
+    //do something
+}
+submit.addEventListener("click", postData);
+
