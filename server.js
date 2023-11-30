ed4acca4dc96f9bd2f928419b3ffe65404baaf15
unknown <bamtorrr@gmail.com>
2023-11-30 14:14:30 +0900

    api 연결 ID/PW 추가

---
+++ server.js
@@ -0,0 +1,9 @@
+// npm install express
+const express = require('express');
+const app = express();
+
+const client_id = '74kfvoj1pv';
+const client_secret = 'rbWvHSBtp3t6oFMtXnimWk3ll30ZTLR8CfnAjj7G';
+
+app.use(express.static('public'));
+
