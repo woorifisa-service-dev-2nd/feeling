7602827c3347ebf5b2d60a8c29196535827a66b9
JohnDohnut <lgj1089@gmail.com>
2023-11-30 15:28:24 +0900

    update

--- public/client.js
+++ public/client.js
@@ -2,6 +2,11 @@ const targetName = document.getElementById('Target_Name');
 const comment = document.getElementById('Comment');
 const submit = document.getElementById('POST');
 const serverURL = '/submitComment';
+const result = document.getElementById('result');
+
+function setResultValue(value) {
+  result.textValue = value;
+}
 
 function postData(event) {
   event.preventDefault();
@@ -15,10 +20,13 @@ function postData(event) {
       comment: comment.value,
     }),
   };
+
   console.log(JSON.stringify(requestMessage));
   fetch(serverURL, requestMessage)
     .then((response) => response.json())
-    .then((data) => console.log('response:', data))
+    // .then((data) => console.log('response:', data))
+    .then((data) => setResultValue(data.body.result))
     .catch((error) => console.log(error));
 }
+
 submit.addEventListener('click', postData);
